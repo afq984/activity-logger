b9b0faf152fe09cd9c6456c430096ce71fff6aa7
Li-Yu Yu <afg984@gmail.com>
2021-12-20 03:43:43 +0800

    add minifyHTML

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -3,12 +3,14 @@ import typescript from '@rollup/plugin-typescript';
 import html from '@web/rollup-plugin-html';
 import summary from 'rollup-plugin-summary';
 import {terser} from 'rollup-plugin-terser';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
 
 export default {
   plugins: [
     html({input: 'index.html'}),
     typescript({tsconfig: 'tsconfig.json', outDir: null}),
     resolve(),
+    minifyHTML(),
     terser({
       module: true,
       warnings: true,
