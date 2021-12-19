96ffc75ec26d55cd9aa4b12e569ae4b79aea94d1
Li-Yu Yu <afg984@gmail.com>
2021-12-19 21:34:53 +0800

    add title

--- src/activity-logger.ts
+++ src/activity-logger.ts
@@ -1,9 +1,12 @@
 import {html, LitElement} from 'lit';
 import {customElement} from 'lit/decorators.js';
+import '@material/mwc-top-app-bar';
 
 @customElement('activity-logger')
 export class ActivityLogger extends LitElement {
   override render() {
-    return html`<p>hello world</p>`;
+    return html`<mwc-top-app-bar>
+      <div slot="title">Activity Logger</div>
+    </mwc-top-app-bar>`;
   }
 }
